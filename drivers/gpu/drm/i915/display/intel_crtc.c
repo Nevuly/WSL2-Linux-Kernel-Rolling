b53ef69a4e83caac0aea69707ab6b6eb71c111f9
Mike Galbraith <umgwanakikbuti@gmail.com>
2016-02-27 09:01:42 +0100

    drm/i915: Don't disable interrupts on PREEMPT_RT during atomic updates
    Commit
       8d7849db3eab7 ("drm/i915: Make sprite updates atomic")
    
    started disabling interrupts across atomic updates. This breaks on PREEMPT_RT
    because within this section the code attempt to acquire spinlock_t locks which
    are sleeping locks on PREEMPT_RT.
    
    According to the comment the interrupts are disabled to avoid random delays and
    not required for protection or synchronisation.
    If this needs to happen with disabled interrupts on PREEMPT_RT, and the
    whole section is restricted to register access then all sleeping locks
    need to be acquired before interrupts are disabled and some function
    maybe moved after enabling interrupts again.
    This includes:
    - prepare_to_wait() + finish_wait() due its wake queue.
    - drm_crtc_vblank_put() -> vblank_disable_fn() drm_device::vbl_lock.
    - skl_pfit_enable(), intel_update_plane(), vlv_atomic_update_fifo() and
      maybe others due to intel_uncore::lock
    - drm_crtc_arm_vblank_event() due to drm_device::event_lock and
      drm_device::vblank_time_lock.
    
    Don't disable interrupts on PREEMPT_RT during atomic updates.
    
    [bigeasy: drop local locks, commit message]
    
    Signed-off-by: Mike Galbraith <umgwanakikbuti@gmail.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- drivers/gpu/drm/i915/display/intel_crtc.c
+++ drivers/gpu/drm/i915/display/intel_crtc.c
@@ -587,7 +587,8 @@ void intel_pipe_update_start(struct intel_atomic_state *state,
 	 */
 	intel_psr_wait_for_idle_locked(new_crtc_state);
 
-	local_irq_disable();
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+		local_irq_disable();
 
 	crtc->debug.min_vbl = evade.min;
 	crtc->debug.max_vbl = evade.max;
@@ -605,7 +606,8 @@ void intel_pipe_update_start(struct intel_atomic_state *state,
 	return;
 
 irq_disable:
-	local_irq_disable();
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+		local_irq_disable();
 }
 
 #if IS_ENABLED(CONFIG_DRM_I915_DEBUG_VBLANK_EVADE)
@@ -750,7 +752,8 @@ void intel_pipe_update_end(struct intel_atomic_state *state,
 	if (!state->base.legacy_cursor_update)
 		intel_vrr_send_push(NULL, new_crtc_state);
 
-	local_irq_enable();
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+		local_irq_enable();
 
 	if (intel_parent_vgpu_active(display))
 		goto out;
