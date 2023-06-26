6748fd5e6a9120608c7d0aaeda16d2343a03dc9e
Yang Jeong Hun <onyxclover9931@gmail.com>
2023-06-26 20:39:29 +0900

    Fix "clocksource/drivers/hyper-v: Include asm/hyperv-tlfs.h not asm/mshyperv.h"
     * Other architectures without X86 still need include 'mshyperv.h' in 'hyperv_timer.h'.
     * This commit fixes e5dfd093eca01a5d8d967f959a2372d7d82eb59c.
    
    Signed-off-by: Yang Jeong Hun <onyxclover9931@gmail.com>

--- drivers/hv/vmbus_drv.c
+++ drivers/hv/vmbus_drv.c
@@ -37,7 +37,11 @@
 #include <linux/dma-map-ops.h>
 #include <linux/pci.h>
 #include <clocksource/hyperv_timer.h>
+
+#ifdef CONFIG_X86
 #include <asm/mshyperv.h>
+#endif
+
 #include "hyperv_vmbus.h"
 
 struct vmbus_dynid {
